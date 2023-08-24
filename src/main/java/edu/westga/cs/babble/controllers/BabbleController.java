b22f4e48af060b629c48e1cf6dd90b729ecd47c8
ab00721 <ab00721@csvlab-vm15.uwg.westga.edu>
2023-08-24 11:43:32 -0400

    Implemented handleTileClick and handleWordClick

--- src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
+++ src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
@@ -5,8 +5,10 @@ import java.util.ResourceBundle;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 import edu.westga.cs.babble.model.EmptyTileBagException;
+import edu.westga.cs.babble.model.PlayedWord;
 import edu.westga.cs.babble.model.Tile;
 import edu.westga.cs.babble.model.TileBag;
+import edu.westga.cs.babble.model.TileNotInGroupException;
 import edu.westga.cs.babble.model.TileRack;
 import edu.westga.cs.babble.model.TileRackFullException;
 import javafx.fxml.FXML;
@@ -37,18 +39,20 @@ public class BabbleController implements Initializable {
     
     private TileRack tileRack;
     private TileBag tileBag;
+    private PlayedWord playedWord;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     	this.tileRack = new TileRack();
     	this.tileBag = new TileBag();
+    	this.playedWord = new PlayedWord();
     	this.createTileRack();
     }
     
     private void createTileRack() {
     	while (this.tileRack.getNumberOfTilesNeeded() > 0) {
     		try {
-				this.tileRack.append(tileBag.drawTile());
+				this.tileRack.append(this.tileBag.drawTile());
 			} catch (TileRackFullException e) {
 				showMessageDialog(null, "Rack Full");
 			} catch (EmptyTileBagException e) {
@@ -56,7 +60,64 @@ public class BabbleController implements Initializable {
 			}
     	}
     	this.gameTiles.setItems(this.tileRack.tiles());
+    	this.setRack();
+    }
+    
+    public void handleTileClick(MouseEvent event) {
+    	Tile selectedTile = gameTiles.getSelectionModel().getSelectedItem();
+    	if (selectedTile != null) {
+    		try {
+    			this.tileRack.remove(selectedTile);
+    			this.playedWord.append(selectedTile);
+    		} catch (TileRackFullException e) {
+				showMessageDialog(null, "Rack Full");
+			} catch (TileNotInGroupException e) {
+				showMessageDialog(null, "Tile Not In Group");
+			}
+    	}
+		this.gameTiles.setItems(this.tileRack.tiles());
+    	this.setRack();
+    	this.word.setItems(this.playedWord.tiles());
+    	this.setWord();
+    }
+    
+    public void handleWordClick(MouseEvent event) {
+    	Tile selectedTile = word.getSelectionModel().getSelectedItem();
+    	if (selectedTile != null) {
+    		try {
+    			this.playedWord.remove(selectedTile);
+    			this.tileRack.append(selectedTile);
+    		} catch (TileRackFullException e) {
+				showMessageDialog(null, "Rack Full");
+			} catch (TileNotInGroupException e) {
+				showMessageDialog(null, "Tile Not In Group");
+			}
+    	}
+		this.gameTiles.setItems(this.tileRack.tiles());
+    	this.setRack();
+		this.word.setItems(this.playedWord.tiles());
+    	this.setWord();
+    }
+    
+    public void play(MouseEvent event) {
+
+    }
+
+    public void reset(MouseEvent event) {
+    	
+    }
+    
+    private void setRack() {
     	this.gameTiles.setCellFactory(new Callback<ListView<Tile>, ListCell<Tile>>() {
+    		@Override
+    		public ListCell<Tile> call(ListView<Tile> tile) {
+    			return new TileCell();
+    		}
+    	});
+    }
+    
+    private void setWord() {
+    	this.word.setCellFactory(new Callback<ListView<Tile>, ListCell<Tile>>() {
     		@Override
     		public ListCell<Tile> call(ListView<Tile> tile) {
     			return new TileCell();
@@ -72,16 +133,6 @@ public class BabbleController implements Initializable {
     			setText(String.valueOf(item.getLetter()));
     		}
     	}
-    }
-    
-    @FXML
-    void play(MouseEvent event) {
-
-    }
-
-    @FXML
-    void reset(MouseEvent event) {
-
     }
     
 }
